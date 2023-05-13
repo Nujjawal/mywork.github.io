a7c6a954be7edbd7b2999effcd1ca777a38883fe
Nujjawal <126577158+Nujjawal@users.noreply.github.com>
2023-05-13 22:14:46 +0530

    my website

---
+++ base/urls.py
@@ -0,0 +1,25 @@
+from django.urls import path
+from . import views
+
+app_name = "base"
+urlpatterns = [
+    path('', views.home,name= "home"),
+    path('home/', views.home,name= "home"),
+    path('DC/', views.DC,name= "DC"),
+    path('Faculty/', views.Faculty,name= "Faculty"),
+    path('Campus/', views.Campus,name= "Campus"),
+    path('Students/', views.Student,name= "Student"),
+    path('Academic/', views.Academic,name= "Academic"),
+    path('principledesk/', views.principledesk,name= "principledesk"),
+    path('Contactus/', views.Contactus,name= "Contactus"),
+    path('Grievance/', views.Grievance,name= "Grievance"),
+    path('index/', views.index,name='index'),
+    path('basic/', views.basic,name='basic'),
+    path('hostel/', views.hostel,name='hostel'),
+    path('library/', views.library,name='library'),
+    path('lab/', views.lab,name='lab'),
+    path('games/', views.games,name='games'),
+    path('infrastructure/', views.infrastructure,name='infrastructure'),
+    path('aboutus/', views.aboutus,name='aboutus'),
+    
+]
---
+++ base/views.py
@@ -0,0 +1,56 @@
+from django.shortcuts import render
+
+# Create your views here.
+def home(request):
+    return render(request,'home.html')
+
+def home(request):
+    return render(request,'home.html')
+
+def DC(request):
+    return render(request,'DC.html')
+
+def Faculty(request):
+    return render(request,'Faculty.html')
+
+def Campus(request):
+    return render(request,'Campus.html')
+
+def Student(request):
+    return render(request,'Student.html')
+
+def Academic(request):
+    return render(request,'Academic.html')
+
+def principledesk(request):
+    return render(request,'principledesk.html')
+
+def Contactus(request):
+    return render(request,'Contactus.html')
+
+def Grievance(request):
+    return render(request,'Grievance.html')
+
+def index(request):
+    return render(request,'index.html')
+
+def basic(request):
+    return render(request,'basic.html')
+
+def hostel(request):
+    return render(request,'hostel.html')
+
+def library(request):
+    return render(request,'library.html')
+
+def lab(request):
+    return render(request,'lab.html')
+
+def games(request):
+    return render(request,'games.html')
+
+def infrastructure(request):
+    return render(request,'infrastructure.html')
+
+def aboutus(request):
+    return render(request,'aboutus.html')
